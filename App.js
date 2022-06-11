0200fdf37118393f04c950e08f0be820b8117ad9
Ewerton Barbosa <ewertonb@live.com>
2022-06-11 01:56:54 -0300

    Criação do projeto Todo

--- App.js
+++ App.js
@@ -1,20 +1,175 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, SafeAreaView, StatusBar, TouchableOpacity, FlatList, Modal } from 'react-native';
+import * as Animatable from 'react-native-animatable'
+
+{/* IMPORT PRA UTILIZAR ICONES */ }
+import { Ionicons } from '@expo/vector-icons'
+
+import TaskList from './src/components/TaskList';
+
+{/* AQUI É CRIADO UM BOTÃO ANIMADO RECEBENDO UM TouchableOpacity QUE É UM BOTÃO PERSONALIZAVEL */ }
+const BtnAnimado = Animatable.createAnimatableComponent(TouchableOpacity);
 
 export default function App() {
+
+  {/* Variavel LIST com os objetos*/ }
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [telaCadastro, setTeclaCadastro] = useState(false);
+
+  function taskAdd(){
+    if (newTask === '') return;
+
+    const data = {
+      key: newTask,
+      task: newTask
+    };
+
+    setTask([...task, data]);
+    setTeclaCadastro(false);
+    setNewTask('');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Minhas Tarefas</Text>
+      </View>
+
+      <FlatList
+        marginHorizontal={10}
+        showsHorizontalScrollIndicator={false}
+        data={task}
+        keyExtractor={(item) => String(item.key)}
+        renderItem={({ item }) => <TaskList data={item} />}
+      />
+
+      {/* Aqui é a tela de cadastro, a propriedade visible recebe a funcao telaCadastro*/}
+      <Modal animationType='slide' transparent={false} visible={telaCadastro}>
+        <SafeAreaView style={styles.telaCad}>
+
+          <View style={styles.telaCadReder}>
+
+            <TouchableOpacity onPress={() => setTeclaCadastro(false)}>
+              <Ionicons style={{ marginLeft: 8, marginRight: 5 }} name='md-arrow-back' size={35} color='#fff' />
+            </TouchableOpacity>
+
+            <Text style={styles.txt}>Nova tarefa</Text>
+
+          </View>
+
+          <Animatable.View style={styles.telaCadBody} animation='fadeInUp' useNativeDriver>
+
+            <TextInput
+              placeholder='Digite sua nova tarefa...'
+              autoCorrect={false}
+              placeholderTextColor='#c9c7c7'
+              style={styles.input}
+              multiline={true}
+              value={newTask}
+              onChangeText={(texto) => setNewTask(texto)}
+            />
+
+            <TouchableOpacity style={styles.btnNovaTarefa} onPress={taskAdd}>
+              <Text style={styles.txtNovaTarefa}>Adicionar</Text>
+            </TouchableOpacity>
+
+          </Animatable.View>
+
+        </SafeAreaView>
+      </Modal>
+
+      {/* No onPress é passada uma funcao anonima alterando o estado do setTeclaCadastro para TRUE assim abrindo a tela*/}
+      <BtnAnimado style={styles.fab}
+        useNativeDriver
+        animation='bounceInUp'
+        duration={1500}
+        onPress={() => setTeclaCadastro(true)}
+      >
+        <Ionicons name="ios-add" size={35} color='#fff' />
+      </BtnAnimado>
+
+      {/* AQUI HABILITA O AJUSTE AUTOMATICO DA STATUSBAR RECORTE DA CAMERA */}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
+
+{/* AQUI FICA OS ESTILOS DOS COMPONENTES*/}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000'
+  },
+  title: {
+    color: '#ffffff',
+    marginTop: 20,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: 'center'
+  },
+  fab: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    backgroundColor: '#0080c0',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 30,
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: '#c0c0c0',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 1,
+      height: 3
+    }
   },
+  telaCad: {
+    backgroundColor: '#000000',
+    flex: 1
+  },
+  telaCadReder: {
+    marginLeft: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  txt: {
+    color: '#fff',
+    fontSize: 25
+  },
+  telaCadBody: {
+    marginTop: 15,
+  },
+  input: {
+    fontSize: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 30,
+    backgroundColor: '#fff',
+    padding: 9,
+    height: 80,
+    textAlignVertical: 'top',
+    color: '#000',
+    borderRadius: 10,
+    fontSize: 20
+  },
+  btnNovaTarefa: {
+    backgroundColor: '#fff',
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 10,
+    marginLeft: 10,
+    borderRadius: 5,
+    height: 40
+  },
+  txtNovaTarefa: {
+    fontSize: 20
+  }
 });
---
+++ src/components/TaskList/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import {Ionicons} from '@expo/vector-icons'
+import * as Animatable from 'react-native-animatable'
+
+export default function TaskList({data}){
+    return(
+        <Animatable.View style={styles.container}
+        animation='bounceIn'
+        >
+            <TouchableOpacity>
+                <Ionicons name='md-checkmark-circle' size={30} color='#121212'/>
+            </TouchableOpacity>
+
+            <View>
+                <Text style={styles.txt}>{data.task}</Text>
+            </View>
+            
+        </Animatable.View>   
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        margin: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 7,
+        elevation: 1.5,
+        shadowColor: '#00ff00',
+        shadowOpacity: 0.2,
+        textShadowOffset: {
+            width: 1,
+            height: 3
+        }
+    },
+    txt:{
+        color: '#000000',
+        fontSize: 20,
+        paddingLeft: 10,
+        paddingRight: 15
+    }
+})
